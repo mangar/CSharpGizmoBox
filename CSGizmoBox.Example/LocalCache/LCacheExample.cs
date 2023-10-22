83f04a64349a5c3f34fdac1eaa198219d389de96
Marcio Mangar <marcio.mangar@gmail.com>
2023-10-22 10:56:06 -0300

    Int Range Validation

--- CSGizmoBox.Example/LocalCache/LCacheExample.cs
+++ CSGizmoBox.Example/LocalCache/LCacheExample.cs
@@ -8,47 +8,47 @@ namespace CSGizmoBox.LocalCache
 {
     public class LCacheExample
     {
-        static void Main(string[] args) {
+        //static void Main(string[] args) {
 
-            LCache.Set("1", "11");
+        //    LCache.Set("1", "11");
 
-            var content = LCache.Get("1");
-            Console.WriteLine($"Get(1): {content}");
-
-
-            content = LCache.Get("2");
-            Console.WriteLine($"Get(2): {content}");
+        //    var content = LCache.Get("1");
+        //    Console.WriteLine($"Get(1): {content}");
 
 
-            content = LCache.Get("3", defaultValue:"3>>3", addIfNotFound:true);
-            Console.WriteLine($"Get(3): {content}");
+        //    content = LCache.Get("2");
+        //    Console.WriteLine($"Get(2): {content}");
 
 
-            LCache.Set("4", "4 >> 1 second", 1);
-
-            content = LCache.Get("4");
-            Console.WriteLine($"Get(4): {content}");
+        //    content = LCache.Get("3", defaultValue:"3>>3", addIfNotFound:true);
+        //    Console.WriteLine($"Get(3): {content}");
 
 
-            Console.WriteLine(">> Dump <<");
-            Console.WriteLine(LCache.Dump());
+        //    LCache.Set("4", "4 >> 1 second", 1);
+
+        //    content = LCache.Get("4");
+        //    Console.WriteLine($"Get(4): {content}");
 
 
-
-            Console.WriteLine("Sleeping 2 seconds..................................");
-            Thread.Sleep(2000);
+        //    Console.WriteLine(">> Dump <<");
+        //    Console.WriteLine(LCache.Dump());
 
 
 
-            content = LCache.Get("4");
-            Console.WriteLine($"Get(4): {content}");
-
-
-            Console.WriteLine(">> Dump <<");
-            Console.WriteLine(LCache.Dump());
+        //    Console.WriteLine("Sleeping 2 seconds..................................");
+        //    Thread.Sleep(2000);
 
 
 
-        }
+        //    content = LCache.Get("4");
+        //    Console.WriteLine($"Get(4): {content}");
+
+
+        //    Console.WriteLine(">> Dump <<");
+        //    Console.WriteLine(LCache.Dump());
+
+
+
+        //}
     }
 }
---
+++ CSGizmoBox/Entity/Validation/Generics/RangeValidation.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+namespace CSGizmoBox.Entity.Validation.Generics
+{
+    public class RangeValidation
+    {
+        public static ValidationVO Validate(int content, List<int> range, string Codigo = "ERROR-RANGE", string Message = "Valor do campo fora da faixa.")
+        {
+
+            if (content == null || range.Count == 0 || !range.Contains(content))
+            {
+                return new ValidationVO()
+                {
+                    IsValid = false,
+                    Codigo = Codigo,
+                    Message = Message
+                };
+            }
+            else { 
+                return ValidationVO.Empty();
+            } 
+
+        }
+
+    }
+}
